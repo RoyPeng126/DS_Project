a2383f70e798cb64bc34065ec5bac93eeed31ee0
RoyPeng126 <147261778+RoyPeng126@users.noreply.github.com>
2024-12-22 14:18:27 +0800

    IOSTest1

---
+++ G9IOSDSProject/G9IOSDSProject/G9IOSDSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  G9IOSDSProjectApp.swift
+//  G9IOSDSProject
+//
+//  Created by 彭宇承 on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct G9IOSDSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
